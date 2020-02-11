67606ef487f0ae8d2971cc52deb5f5d3f8f75992
sym <1393475526@qq.com>
2020-02-11 17:18:23 +0800

    把本地工程添加到git本地仓库

---
+++ 03_idea-test/src/com/xiaoming/git_test/FirstClass.java
@@ -0,0 +1,21 @@
+package com.xiaoming.git_test;
+
+/**
+ * @author xiaoming
+ * @create 2020-02-11 17:04
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
